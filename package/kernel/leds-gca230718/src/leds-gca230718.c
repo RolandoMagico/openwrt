beb3abc5e5da001984f34223cd5978719c30e369
Roland Reinl <reinlroland+github@gmail.com>
2023-12-27 10:08:13 +0100

    Reduced I2C communication to required data only

--- package/kernel/leds-gca230718/src/leds-gca230718.c
+++ package/kernel/leds-gca230718/src/leds-gca230718.c
@@ -24,6 +24,10 @@
 #define GCA230718_OPMODE_TOGGLE_RAMP_CONTROL_DISABLED           (0x02u)
 #define GCA230718_OPMODE_TOGGLE_RAMP_CONTROL_ENSABLED           (0x03u)
 
+#define GCA230718_1ST_SEQUENCE_BYTE_1                           (0x02u)
+#define GCA230718_2ND_SEQUENCE_BYTE_1                           (0x01u)
+#define GCA230718_3RD_SEQUENCE_BYTE_1                           (0x03u)
+
 struct gca230718_led
 {
 	enum led_brightness brightness;
@@ -46,6 +50,44 @@ static void gca230718_init_private_led_data(struct gca230718_private* data)
 	}
 }
 
+static void gca230718_send_sequence(struct i2c_client *client, u8 byte0, struct gca230718_private* gca230718_privateData)
+{
+	int status = 0;
+	u8 ledIndex;
+	const u8 resetCommand[2] = { 0x81, 0xE4 };
+	const u8 resetCommandRegister = 0x00;
+
+	u8 controlCommand[13];
+	const u8 controlCommandRegister = 0x03;
+
+	controlCommand[0] = 0x0C; /* Unknown */
+	controlCommand[1] = byte0;
+	controlCommand[2] = GCA230718_OPMODE_NO_TOGGLE;
+	/* Byte 3-6 are set below to the brighness value of the individual LEDs */
+	controlCommand[7] = 0x01; /* Frequency, doesn't care as long as GCA230718_OPMODE_NO_TOGGLE is used above */
+	/* Byte 8-11 are set below to the brighness value of the individual LEDs */
+	controlCommand[12] = 0x87;
+
+	for (ledIndex = 0; ledIndex < GCA230718_MAX_LEDS; ledIndex++)
+	{
+		controlCommand[3 + ledIndex] = gca230718_privateData->leds[ledIndex].brightness;
+		controlCommand[8 + ledIndex] = gca230718_privateData->leds[ledIndex].brightness;
+	}
+
+	mutex_lock(&(gca230718_privateData->lock));
+
+	if ((status = i2c_smbus_write_i2c_block_data(client, resetCommandRegister, sizeof(resetCommand), resetCommand)) != 0)
+	{
+		pr_info("Error %i during call of i2c_smbus_write_i2c_block_data for reset command\n", status);
+	}
+	else if ((status = i2c_smbus_write_i2c_block_data(client, controlCommandRegister, sizeof(controlCommand), controlCommand)) != 0)
+	{
+		pr_info("Error %i during call of i2c_smbus_write_i2c_block_data for control command\n", status);
+	}
+
+	mutex_unlock(&(gca230718_privateData->lock));
+}
+
 static int gca230718_set_brightness(struct led_classdev *led_cdev, enum led_brightness value)
 {
 	struct gca230718_led* led;
@@ -56,52 +98,14 @@ static int gca230718_set_brightness(struct led_classdev *led_cdev, enum led_brig
 
 	if (client != NULL)
 	{
-		u8 ledIndex;
-		u8 loopIndex;
-		struct gca230718_private* data;
-
-		const u8 resetCommand[2] = { 0x81, 0xE4 };
-		const u8 resetCommandRegister = 0x00;
-
-		u8 controlCommand[13];
-		const u8 controlCommandRegister = 0x03;
-		const u8 controlCommandByte1Sequence[3] = { 0x02, 0x01, 0x03 };
+		struct gca230718_private* gca230718_privateData;
 
 		led->brightness = value;
-		data = i2c_get_clientdata(client);
+		gca230718_privateData = i2c_get_clientdata(client);
 
-		controlCommand[0] = 0x0C; /* Unknown */
-		/* Byte 1 is set below to the values from controlCommandByte1Sequence */
-		controlCommand[2] = GCA230718_OPMODE_NO_TOGGLE;
-		/* Byte 3-6 are set below to the brighness value of the individual LEDs */
-		controlCommand[7] = 0x01; /* Frequency, doesn't care as long as GCA230718_OPMODE_NO_TOGGLE is used above */
-		/* Byte 8-11 are set below to the brighness value of the individual LEDs */
-		controlCommand[12] = 0x87;
-
-		for (ledIndex = 0; ledIndex < GCA230718_MAX_LEDS; ledIndex++)
-		{
-			controlCommand[3 + ledIndex] = data->leds[ledIndex].brightness;
-			controlCommand[8 + ledIndex] = data->leds[ledIndex].brightness;
-		}
-
-		mutex_lock(&data->lock);
-
-		for (loopIndex = 0; loopIndex < 3; loopIndex++)
-		{
-			int status = 0;
-			controlCommand[1] = controlCommandByte1Sequence[loopIndex];
-			if ((status = i2c_smbus_write_i2c_block_data(client, resetCommandRegister, sizeof(resetCommand), resetCommand)) != 0)
-			{
-				pr_info("Error %i during call of i2c_smbus_write_i2c_block_data for reset command in loop sequence %i\n", status, loopIndex);
-			}
-			else if ((status = i2c_smbus_write_i2c_block_data(client, controlCommandRegister, sizeof(controlCommand), controlCommand)) != 0)
-			{
-				pr_info("Error %i during call of i2c_smbus_write_i2c_block_data for control command in loop sequence %i\n", status, loopIndex);
-			}
-		}
-
-		mutex_unlock(&data->lock);
+		gca230718_send_sequence(client, GCA230718_2ND_SEQUENCE_BYTE_1, gca230718_privateData);
 	}
+
 	return 0;
 }
 
@@ -162,6 +166,17 @@ static int gca230718_probe(struct i2c_client *client, const struct i2c_device_id
 				}
 			}
 		}
+	}
+
+	if (status == 0)
+	{
+		/* 
+		Send full initialization sequence.
+		Afterwards only GCA230718_2ND_SEQUENCE_BYTE_1 must be send to upddate the brightness values.
+		*/
+		gca230718_send_sequence(client, GCA230718_1ST_SEQUENCE_BYTE_1, gca230718_privateData);
+		gca230718_send_sequence(client, GCA230718_2ND_SEQUENCE_BYTE_1, gca230718_privateData);
+		gca230718_send_sequence(client, GCA230718_3RD_SEQUENCE_BYTE_1, gca230718_privateData);
 	}
 
 	return status;
