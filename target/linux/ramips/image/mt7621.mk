f50e438ec73f171430ec527e1ab92e41ea2e521b
Roland Reinl <reinlroland+github@gmail.com>
2024-05-28 17:11:08 +0200

    Boot after sysupgrade fails:
    fw1_version=6, fw1_status=0
    fw2_version=5, fw2_status=2
    ******** try booting from firmware1 count=0 ********
    fw1_length is NULL, skip check firmware1
    Loading FIT image at offset 0xd00000 to memory 0x80010000, size 0x3066f8 ...
    Automatic boot of image at addr 0x80010000 ...
       Using 'config-1' configuration
       Trying 'kernel-1' kernel subimage
         Description:  MIPS OpenWrt Linux-6.6.30
         Type:         Kernel Image
         Compression:  lzma compressed
         Data Start:   0x800100e4
         Data Size:    3157821 Bytes = 3 MiB
         Architecture: MIPS
         OS:           Linux
         Load Address: 0x80010000
         Entry Point:  0x80010000
         Hash algo:    crc32
         Hash value:   912787ff
         Hash algo:    sha1
         Hash value:   6461bb427c64953e4109bfd7ed5a5e882070e5a3
       Verifying Hash Integrity ... crc32+ sha1+ OK
       Using 'config-1' configuration
       Trying 'fdt-1' fdt subimage
         Description:  MIPS OpenWrt tplink_hc220-g5-v1 device tree blob
         Type:         Flat Device Tree
         Compression:  uncompressed
         Data Start:   0x80313168
         Data Size:    12358 Bytes = 12.1 KiB
         Architecture: MIPS
         Hash algo:    crc32
         Hash value:   34f4b8f2
         Hash algo:    sha1
         Hash value:   45656bb98eb48e8f44aebf7a09b9b4ccfdb002f6
       Verifying Hash Integrity ... crc32+ sha1+ OK
       Booting using the fdt blob at 0x80313168
       Uncompressing Kernel Image ... lzma compressed: uncompress error 1

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2763,6 +2763,8 @@ define Device/tplink_hc220-g5-v1
   DEVICE_MODEL := HC220-G5
   DEVICE_VARIANT := v1
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt7615e kmod-mt7663-firmware-ap uboot-envtools
+  KERNEL_SIZE := 8192k
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb | pad-to 64k
   KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | loader-kernel | uImage none
   KERNEL_LOADADDR := 0x80010000
   TPLINK_BOARD_ID := HC220-G5V1
